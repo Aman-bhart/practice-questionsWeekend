675787b4e479d43f0772b954a435dde69183f489
amanKumarBharti <183721382+Aman-bhart@users.noreply.github.com>
2024-12-22 14:54:40 +0530

    added truthValesOf

--- map.js
+++ map.js
@@ -51,11 +51,17 @@ const firstCharactersOf = function (strings) {
 
 // truth values of [0, 1, 2, 3] => [false, true, true, true]
 // Assume non-zero numbers are true, and zero is false
-const convertsToBollean = function()
+const convertsToBollean = function (number) {
+  return number !== 0;
+};
 
 const truthValuesOf = function (numbers) {
   return numbers.map(convertsToBollean);
 };
+
+// console.log(truthValuesOf([1, 2, 3, 0, 1, 0]));
+// console.log(truthValuesOf([1, 2, -3, 0, 1, 0]));
+
 
 // reverse strings of ["hello", "world"] => ["olleh", "dlrow"]
 const reversedStringsOf = function (strings) { };
