3c916d58da599cd969aa3b7b4e2cb7ba69a91466
amanKumarBharti <183721382+Aman-bhart@users.noreply.github.com>
2024-12-23 00:31:30 +0530

    #07 added filterIncompleteProfiles function

--- filter/filter.js
+++ filter/filter.js
@@ -45,7 +45,11 @@ const filterLongBooks = function (books) {
 // console.log(filterLongBooks([{ title: "Book 1", pages: 150 }, { title: "Book 2", pages: 250 }]));
 
 // users with incomplete profiles [{username: "alice", profileComplete: true}, {username: "bob", profileComplete: false}] => [{username: "bob", profileComplete: false}]
-const filterIncompleteProfiles = function (users) { };
+const filterIncompleteProfiles = function (users) {
+  return users.filter(function (user) { return !user.profileComplete; });
+};
+
+// console.log(filterIncompleteProfiles([{ username: "alice", profileComplete: true }, { username: "bob", profileComplete: false }]));
 
 // students with grades above 80 [{name: "John", grade: 75}, {name: "Jane", grade: 85}] => [{name: "Jane", grade: 85}]
 const filterHighGrades = function (students) { };
